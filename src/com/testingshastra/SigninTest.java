efb3bab32c0f8de80ee0d9d688156b07cced1bf1
harshalkadgale <harshalkadgale@gmail.com>
2022-04-17 14:35:28 +0530

    adding SignInTest class

---
+++ src/com/testingshastra/SigninTest.java
@@ -0,0 +1,12 @@
+package com.testingshastra;
+
+import org.testng.annotations.Test;
+
+public class SigninTest {
+	@Test
+	public void firstTestCase() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
